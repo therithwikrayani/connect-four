df6b20647c5d4095b9f831fa361b606eecae5b8f
Rithwik Rayani <therithwikrayani@gmail.com>
2023-04-02 18:37:00 -0500

    changed board to 1d array, added win checker, added board scoring function for use with minimax algorithm

--- prototype/main.cpp
+++ prototype/main.cpp
@@ -5,30 +5,43 @@ using namespace std;
 int displayBoard();
 int resetBoard();
 int insertToken(int column);
+int checkWin();
+int index(int row, int col);
+int scoreBoard();
 
 // initializing global variables
-int BOARD[6][7] = {0};
+int BOARD[42] = {0};
 int TURN = 1;
 
+
 int main() {
-    insertToken(4);
-    insertToken(3);
-    insertToken(3);
-    displayBoard();
+    while (true)
+    {
+        int input = 0;
+        displayBoard();
+        cout << "Player " << TURN << " choose a column ";
+        cin >> input;
+        insertToken(input);
+        checkWin();
+    }
     return 0;
+}
+
+int index(int row, int col) {
+    return row * 7 + col;
 }
 
 int displayBoard() {
     cout << endl;
     for (int i = 0; i < 6; i++) {
         for (int j = 0; j < 7; j++) {
-            cout << BOARD[i][j] << " ";
+            cout << BOARD[index(i, j)] << " ";
         }
         cout << endl;
     }
     cout << endl;
 
-    for (int i = 1; i <= 7; i++) {
+    for (int i = 0; i < 7; i++) {
         cout << i << " ";
     }
     cout << endl;
@@ -37,23 +50,141 @@ int displayBoard() {
 }
 
 int resetBoard() {
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 7; j++) {
-            BOARD[i][j] = 0;
-        }
+    for (int i = 0; i < 42; i++) {
+        BOARD[i] = 0;
     }
+    TURN = 1;
     return 0;
 }
 
 int insertToken(int column) {
+    if (column < 0 || column > 6) {
+        cout << "Please insert a valid column." << endl;
+        return 1;
+    }
+    
     for (int i = 5; i >= 0; i--) {
-        if (BOARD[i][column] == 0) {
-            BOARD[i][column] = TURN;
+        if (BOARD[index(i, column)] == 0) {
+            BOARD[index(i, column)] = TURN;
             if (TURN == 1) {TURN = 2;}
             else {TURN = 1;}
             return 0;
         }
     }
+
+    cout << "Cannot insert a token in this column." << endl;
     return 1;
 }
 
+int checkWin() {
+    for (int row = 0; row < 6; row++) {
+        for (int col = 0; col < 7; col++) {
+            int current = BOARD[index(row, col)];
+            if (current == 0) {
+                continue;
+            }
+
+            // Check horizontal
+            if (col + 3 < 7 &&
+                current == BOARD[index(row, col + 1)] &&
+                current == BOARD[index(row, col + 2)] &&
+                current == BOARD[index(row, col + 3)]) {
+                cout << "Player " << current << " wins!" << endl;
+                exit(0);
+            }
+
+            // Check vertical
+            if (row + 3 < 6 &&
+                current == BOARD[index(row + 1, col)] &&
+                current == BOARD[index(row + 2, col)] &&
+                current == BOARD[index(row + 3, col)]) {
+                cout << "Player " << current << " wins!" << endl;
+                exit(0);
+            }
+
+            // Check diagonal down-right
+            if (row + 3 < 6 && col + 3 < 7 &&
+                current == BOARD[index(row + 1, col + 1)] &&
+                current == BOARD[index(row + 2, col + 2)] &&
+                current == BOARD[index(row + 3, col + 3)]) {
+                cout << "Player " << current << " wins!" << endl;
+                exit(0);
+            }
+
+            // Check diagonal up-right
+            if (row - 3 >= 0 && col + 3 < 7 &&
+                current == BOARD[index(row - 1, col + 1)] &&
+                current == BOARD[index(row - 2, col + 2)] &&
+                current == BOARD[index(row - 3, col - 3)]) {
+                cout << "Player " << current << " wins!" << endl;
+                exit(0);
+            }
+        }
+    }
+    return 0;
+}
+
+int scoreBoard() {
+    int totalScore = 0;
+    int scores[] = {0, 1, 10, 1000, 100000};
+
+    // Check rows
+    for (int row = 0; row < 6; row++) {
+        for (int col = 0; col < 4; col++) {
+            int count1 = 0, count2 = 0;
+            for (int i = 0; i < 4; i++) {
+                if (BOARD[index(row, col + i)] == 1) {
+                    count1++;
+                } else if (BOARD[index(row, col + i)] == 2) {
+                    count2++;
+                }
+            }
+            totalScore += scores[count1] - scores[count2];
+        }
+    }
+
+    // Check columns
+    for (int col = 0; col < 7; col++) {
+        for (int row = 0; row < 3; row++) {
+            int count1 = 0, count2 = 0;
+            for (int i = 0; i < 4; i++) {
+                if (BOARD[index(row + i, col)] == 1) {
+                    count1++;
+                } else if (BOARD[index(row + i, col)] == 2) {
+                    count2++;
+                }
+            }
+            totalScore += scores[count1] - scores[count2];
+        }
+    }
+
+    // Check diagonals down-right
+    for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < 4; col++) {
+            int count1 = 0, count2 = 0;
+            for (int i = 0; i < 4; i++) {
+                if (BOARD[index(row + i, col + i)] == 1) {
+                    count1++;
+                } else if (BOARD[index(row + i, col + i)] == 2) {
+                    count2++;
+                }
+            }
+            totalScore += scores[count1] - scores[count2];
+        }
+    }
+
+    // Check diagonals up-right
+    for (int row = 3; row < 6; row++) {
+        for (int col = 0; col < 4; col++) {
+            int count1 = 0, count2 = 0;
+            for (int i = 0; i < 4; i++) {
+                if (BOARD[index(row - i, col + i)] == 1) {
+                    count1++;
+                } else if (BOARD[index(row - i, col + i)] == 2) {
+                    count2++;
+                }
+            }
+            totalScore += scores[count1] - scores[count2];
+        }
+    }
+}
